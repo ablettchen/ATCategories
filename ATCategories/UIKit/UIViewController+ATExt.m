c95fb5f2dad06b9081bbb47c2e4e3ea606141e4e
ablett <ablettchen@gmail.com>
2019-11-10 15:40:48 +0800

    mod

--- ATCategories/UIKit/UIViewController+ATExt.m
+++ ATCategories/UIKit/UIViewController+ATExt.m
@@ -13,30 +13,8 @@
 - (void)at_setInterfaceOrientation:(enum UIInterfaceOrientation)interfaceOrientation {
     
     if(![[UIDevice currentDevice] respondsToSelector:@selector(setOrientation:)]) {return;}
-    
-    if (self.supportedInterfaceOrientations == UIInterfaceOrientationMaskPortrait) {
-        if (interfaceOrientation != UIInterfaceOrientationPortrait) {return;}
-    }else if (self.supportedInterfaceOrientations == UIInterfaceOrientationMaskLandscapeLeft) {
-        if (interfaceOrientation != UIInterfaceOrientationLandscapeLeft) {return;}
-    }else if (self.supportedInterfaceOrientations == UIInterfaceOrientationMaskLandscapeRight) {
-        if (interfaceOrientation != UIInterfaceOrientationLandscapeRight) {return;}
-    }else if (self.supportedInterfaceOrientations == UIInterfaceOrientationMaskPortraitUpsideDown) {
-        if (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown) {return;}
-    }else if (self.supportedInterfaceOrientations == UIInterfaceOrientationMaskLandscape) {
-        if (interfaceOrientation != UIInterfaceOrientationLandscapeLeft ||
-            interfaceOrientation != UIInterfaceOrientationLandscapeRight) {return;}
-    }else if (self.supportedInterfaceOrientations == UIInterfaceOrientationMaskAll) {
-        if (interfaceOrientation != UIInterfaceOrientationLandscapeLeft ||
-            interfaceOrientation != UIInterfaceOrientationLandscapeRight ||
-            interfaceOrientation != UIInterfaceOrientationPortrait ||
-            interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown) {return;}
-    }else if (self.supportedInterfaceOrientations == UIInterfaceOrientationMaskAllButUpsideDown) {
-        if (interfaceOrientation != UIInterfaceOrientationLandscapeLeft ||
-            interfaceOrientation != UIInterfaceOrientationLandscapeRight ||
-            interfaceOrientation != UIInterfaceOrientationPortrait ) {return;}
-    }
-    
-    enum UIInterfaceOrientation *unknow = UIInterfaceOrientationUnknown;
+
+    enum UIInterfaceOrientation unknow = UIInterfaceOrientationUnknown;
     SEL selector = NSSelectorFromString(@"setOrientation:");
     NSInvocation *invocation = [NSInvocation invocationWithMethodSignature:[UIDevice instanceMethodSignatureForSelector:selector]];
     [invocation setSelector:selector];
@@ -50,6 +28,7 @@
 @end
 
 @implementation UITabBarController (ATExt)
+
 
 - (BOOL)shouldAutorotate {
     UIViewController *vc = self.viewControllers[self.selectedIndex];
@@ -82,5 +61,4 @@
 }
 
 @end
-
 
--- Example/ATCategories/ATViewController.m
+++ Example/ATCategories/ATViewController.m
@@ -11,7 +11,7 @@
 #import <ATCategories.h>
 
 @interface ATViewController ()
-
+@property (strong, nonatomic) UILabel *titleLabel;
 @end
 
 @implementation ATViewController
@@ -19,6 +19,27 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    self.titleLabel = [UILabel new];
+    self.titleLabel.text = @"LandscapeRight";
+    self.titleLabel.textAlignment = NSTextAlignmentCenter;
+    [self.view addSubview:self.titleLabel];
+    
+}
+
+- (void)viewDidLayoutSubviews {
+    [super viewDidLayoutSubviews];
+    self.titleLabel.at_size = CGSizeMake(200, 40);
+    self.titleLabel.center = self.view.center;
+}
+
+- (UIInterfaceOrientationMask)supportedInterfaceOrientations {
+    return UIInterfaceOrientationMaskLandscapeRight;
+}
+
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+    [self at_setInterfaceOrientation:UIInterfaceOrientationLandscapeRight];
 }
 
 - (void)didReceiveMemoryWarning {
